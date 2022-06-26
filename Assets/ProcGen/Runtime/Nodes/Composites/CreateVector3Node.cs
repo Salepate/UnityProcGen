a820273c6610fab11e1bb8102adb98349b8ab2f6
Salepate <salepate@gmail.com>
2022-06-26 14:53:14 +0200

    add missing z input to Create Vector3

--- Assets/ProcGen/Runtime/Nodes/Composites/CreateVector3Node.cs
+++ Assets/ProcGen/Runtime/Nodes/Composites/CreateVector3Node.cs
@@ -11,7 +11,7 @@ namespace ProcGen.Nodes.Composites
 
         public override void Initialize()
         {
-            Inputs = NodeConnector.CreateInputs(ConnectorType.Float, ConnectorType.Float);
+            Inputs = NodeConnector.CreateInputs(ConnectorType.Float, ConnectorType.Float, ConnectorType.Float);
             Outputs = NodeOutput.CreateOutputs(ConnectorType.Vector2);
         }
 
