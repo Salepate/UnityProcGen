50b15a68468f399e30ccee95c5d1bf487996c5fb
Salepate <salepate@gmail.com>
2022-06-26 11:42:05 +0200

    update sample performances

--- Assets/Samples/Behaviours/DungeonTileSpawner.cs
+++ Assets/Samples/Behaviours/DungeonTileSpawner.cs
@@ -1,6 +1,4 @@
 using ProcGen;
-using ProcGen.Serialization;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,77 +7,58 @@ namespace ProcGenSamples
     public class DungeonTileSpawner : MonoBehaviour
     {
         public GenerativeGraphInstance Graph;
+        public int TilesPerSide = 50;
+        // cache
+        private List<GameObject> m_Tiles;
+        // graph queries
         private TileMapNode m_Tilemap;
         private DungeonTileNode m_DungeonTile;
 
-        public int TileSize = 50;
-        private int m_X;
-        private int m_Y;
-
-        private List<GameObject> m_Tiles;
-
-        void Start()
+        private void Awake()
         {
+            AllocateTiles();
             Graph.GenerateRuntime();
+        }
+        private void Start()
+        {
             m_Tilemap = Graph.Runtime.Query<TileMapNode>();
             m_DungeonTile = Graph.Runtime.Query<DungeonTileNode>();
+            UpdateGrid();
+            Graph.OnGraphUpdate += OnGraphChange; // only invoked in editor
+        }
+
+        private void AllocateTiles()
+        {
             m_Tiles = new List<GameObject>();
-            UpdateGrid(0, 0);
-            Graph.OnGraphUpdate += OnGraphChange;
+            Quaternion baseRot = Quaternion.Euler(90f, 0f, 0f);
+            for (int i = 0; i < TilesPerSide * TilesPerSide; ++i)
+            {
+                GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                tile.transform.localRotation = baseRot;
+                m_Tiles.Add(tile);
+            }
         }
 
         private void OnGraphChange()
         {
-            UpdateGrid(m_X, m_Y); // refresh
+            UpdateGrid();
         }
 
-        private void OnGUI()
+        private void UpdateGrid()
         {
-            GUILayout.BeginVertical(GUI.skin.box);
-            GUILayout.BeginHorizontal();
-
-            if (GUILayout.Button("<", GUILayout.Width(20f)))
-                UpdateGrid(m_X - 1, m_Y);
-            GUILayout.Label($"X: {m_X}", GUILayout.Width(50f));
-            if (GUILayout.Button(">", GUILayout.Width(20f)))
-                UpdateGrid(m_X + 1, m_Y);
-
-            GUILayout.EndHorizontal();
-            GUILayout.BeginHorizontal();
-
-            if (GUILayout.Button("<", GUILayout.Width(20f)))
-                UpdateGrid(m_X, m_Y-1);
-            GUILayout.Label($"Y: {m_Y}", GUILayout.Width(50f));
-            if (GUILayout.Button(">", GUILayout.Width(20f)))
-                UpdateGrid(m_X, m_Y+1);
-
-            GUILayout.EndHorizontal();
-            GUILayout.EndVertical();
-        }
-
-        private void UpdateGrid(int x, int y)
-        {
-            m_X = x;
-            m_Y = y;
-
-            for (int i = 0; i < m_Tiles.Count; ++i)
-                Destroy(m_Tiles[i]);
-
-            m_Tiles.Clear();
-
-            for (int i = 0; i < TileSize; ++i)
+            for(int i = 0; i < m_Tiles.Count; ++i)
             {
-                for(int j = 0; j < TileSize; ++j)
+                int x = i % TilesPerSide;
+                int y = i / TilesPerSide;
+                GameObject tile = m_Tiles[i];
+                m_Tilemap.m_Coordinate = new Vector2Int(x,y);
+                Graph.Runtime.EvaluateNode(m_DungeonTile);
+
+                tile.transform.localScale = m_DungeonTile.IsTile ? Vector3.one : Vector3.zero; 
+
+                if (m_DungeonTile.IsTile)
                 {
-                    m_Tilemap.m_Coordinate = new Vector2Int(x * TileSize + i, y * TileSize + j);
-                    Graph.Runtime.EvaluateNode(m_DungeonTile);
-                    if ( m_DungeonTile.IsTile )
-                    {
-                        var prim = GameObject.CreatePrimitive(PrimitiveType.Quad);
-                        prim.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
-                        prim.transform.position = new Vector3(i - (TileSize) / 2f, 0f, j - (TileSize)/2f);
-                        m_Tiles.Add(prim);
-                    }
+                    tile.transform.localPosition = new Vector3(x - (TilesPerSide) / 2f, 0f, y - (TilesPerSide) / 2f);
                 }
             }
         }
