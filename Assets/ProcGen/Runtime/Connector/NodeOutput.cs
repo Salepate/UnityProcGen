1debfae40a88792bf29c68f9c31d6a79d8195d81
Salepate <salepate@gmail.com>
2022-06-30 13:19:59 +0200

    simple helper for transmitter nodes

--- Assets/ProcGen/Runtime/Connector/NodeOutput.cs
+++ Assets/ProcGen/Runtime/Connector/NodeOutput.cs
@@ -21,5 +21,23 @@ namespace ProcGen.Connector
         {
             ConnectorType = connectorType;
         }
+
+        internal void Write(ref NodeInput input, ConnectorType type, bool rewriteOutputType = false)
+        {
+            if (rewriteOutputType)
+                ConnectorType = type;
+
+            switch (type)
+            {
+                case ConnectorType.Integer: ValueInt = input.ReadInteger();
+                    break;
+                case ConnectorType.Float: ValueFloat = input.ReadFloat();
+                    break;
+                case ConnectorType.Vector2: ValueVector2 = input.ReadVector2();
+                    break;
+                case ConnectorType.Vector3: ValueVector3 = input.ReadVector3();
+                    break;
+            }
+        }
     }
 }
